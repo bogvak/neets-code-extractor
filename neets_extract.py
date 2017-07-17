840c04908020977d49504a6145076e140ff7c2da
Bogdan Vakulyuk <bogvak@gmail.com>
2017-07-17 20:24:44 +0300

    Some mimot changes
    - adding support of any controller
    - renamed variables to meet PEP8 conventions
    - adding some of debug console messages

--- neets_extract.py
+++ neets_extract.py
@@ -2,71 +2,87 @@ from lxml import etree
 import os, errno, sys
 
 #Initial vars
-initString = '<?xml version="1.0" encoding="utf-8"?>\n'
-sysCodeXPath = '/Project/ControlSystems/Delta/SystemCode'
+init_string = '<?xml version="1.0" encoding="utf-8"?>\n'
+# sys_code_xpath = '/Project/ControlSystems/Delta/SystemCode'
 
 #Initial defs
-def save_code_to_file(codeDir, element):
-    with open(codeDir+'/'+element.tag+'.pawn', 'w') as file:
+def save_project_file(file_name, full_tree):
+    with open(prj_file_dir + '/'+file_name, 'w', encoding='utf-8') as file:
+        file.write(init_string+etree.tostring(full_tree, pretty_print=True, encoding='unicode'))
+    pass
+
+
+def open_project_file(file_name):
+    full_tree = etree.parse(file_name)
+    return full_tree
+
+
+def save_code_to_file(code_dir, element):
+    with open(code_dir+'/'+element.tag+'.pawn', 'w') as file:
         file.write(element.text)
 
-def decompileNeets(filePath):
 
-    sysCodeDir = prjFileDir + "\\_system_code"
+def decompile_neets(file_path):
+
+    sys_code_dir = prj_file_dir + "\\_system_code"
 
     try:
-        os.makedirs(sysCodeDir)
+        os.makedirs(sys_code_dir)
     except OSError as e:
         if e.errno != errno.EEXIST:
             raise
 
-    tree = open_project_file(filePath)
-    node = tree.xpath(sysCodeXPath)
-    t = node[0].getchildren()
+    full_tree = open_project_file(file_path)
+    node = get_system_code_el(full_tree)
+    t = node.getchildren()
     for node in t:
-        save_code_to_file(sysCodeDir, node)
+        save_code_to_file(sys_code_dir, node)
 
-def compileNeets(filePath):
-    sysCodeDir = prjFileDir + "\\_system_code"
-    if not os.path.exists(sysCodeDir):
+
+def compile_neets(file_path):
+
+    sys_code_dir = prj_file_dir + "\\_system_code"
+
+    if not os.path.exists(sys_code_dir):
         print('no System Code to compile')
         return
-    filesToCompile = os.listdir(sysCodeDir)
 
-    #clearing System Code
-    tree = open_project_file(filePath)
-    delete_all_elements_from_system_code(tree)
+    # clearing System Code
+    full_tree = open_project_file(file_path)
+    delete_all_elements_from_system_code(full_tree)
+    node_system_code = get_system_code_el(full_tree)
 
-    node_system_code = tree.xpath(sysCodeXPath)
+    print("Using "+get_controller_version(full_tree)+" controller")
 
-    for nextFile in filesToCompile:
-        with open(sysCodeDir + '/' + nextFile) as nextFile:
-            file_name, file_extension = os.path.splitext(os.path.basename(nextFile.name))
-            newNode = etree.Element(file_name)
-            newNode.text = nextFile.read()
-            node_system_code[0].append(newNode)
+    files_to_compile = os.scandir(sys_code_dir)
+    for next_file_to_compile in files_to_compile:
+        if not next_file_to_compile.is_dir() :
+            print("Adding "+next_file_to_compile.name)
+            with open(sys_code_dir + '/' + next_file_to_compile.name) as next_file_desc:
+                file_name, file_extension = os.path.splitext(os.path.basename(next_file_desc.name))
+                new_node = etree.Element(file_name)
+                new_node.text = next_file_desc.read()
+                node_system_code.append(new_node)
 
-    save_project_file(os.path.basename(filePath), tree)
+    save_project_file(os.path.basename(file_path), full_tree)
 
-def delete_all_elements_from_system_code(tree):
-    node_system_code = tree.xpath(sysCodeXPath)
-    node_system_code[0].clear()
-    #t = node_system_code[0].getchildren()
-    #for node in t:
-     #   print(node)
-      #  node.clear()
 
-def save_project_file(file_name, tree):
-    with open(prjFileDir + '/'+file_name, 'w', encoding='utf-8') as file:
-        file.write(initString+etree.tostring(tree, pretty_print=True, encoding='unicode'))
-    pass
+def delete_all_elements_from_system_code(full_tree):
+    node_system_code = get_system_code_el(full_tree)
+    node_system_code.clear()
 
-def open_project_file(file_name):
-    tree = etree.parse(file_name)
-    return tree
+
+def get_system_code_el(full_tree):
+    ctr_sys_el = full_tree.getroot().find('ControlSystems').getchildren()[0]
+    sys_code_el = ctr_sys_el.find('SystemCode')
+    return sys_code_el
+
+def get_controller_version(full_tree):
+    ctr_sys_el = full_tree.getroot().find('ControlSystems').getchildren()[0]
+    return ctr_sys_el.tag
 
 #--- Main procedure
-#Parsing command line input
+#--- Parsing command line input
 if len(sys.argv) < 3:
     print("too less arguments")
     sys.exit(0)
@@ -75,15 +91,15 @@ if (sys.argv[1] != "-dec") and (sys.argv[1] != "-com"):
     print("no commands found")
     sys.exit(0)
 
-prjFileDir = sys.path[0]
-prjFilePath = prjFileDir + "\\" + sys.argv[2]
+prj_file_dir = sys.path[0]
+prj_file_path = prj_file_dir + "\\" + sys.argv[2]
 
-if not os.path.isfile(prjFilePath):
+if not os.path.isfile(prj_file_path):
     print("wrong file")
     sys.exit(0)
 
 if sys.argv[1] == "-dec":
-    decompileNeets(prjFilePath)
+    decompile_neets(prj_file_path)
 
 if sys.argv[1] == "-com":
-    compileNeets(prjFilePath)
+    compile_neets(prj_file_path)
